768eff8e185dc171c30ac3d926552ed34214f54c
Kevin Schmid <schmid.kevin.manuel@gmail.com>
2020-06-08 18:03:51 +0200

    project setup

---
+++ app/src/main/java/at/fhjoanneum/picturl/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package at.fhjoanneum.picturl.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import at.fhjoanneum.picturl.R
+
+class SplashActivity : AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash);
+    }
+}
