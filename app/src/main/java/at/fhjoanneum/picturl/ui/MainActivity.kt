8e4d2fb08610404aa7bea8086e3018415bad9f03
Kevin Schmid <schmid.kevin.manuel@gmail.com>
2020-06-19 16:26:52 +0200

    added camera functionality

--- app/src/main/java/at/fhjoanneum/picturl/ui/MainActivity.kt
+++ app/src/main/java/at/fhjoanneum/picturl/ui/MainActivity.kt
@@ -4,40 +4,97 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.view.Menu
 import android.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import at.fhjoanneum.picturl.MAIN_ACTIVITY_RESULT_PICK_IMAGE
+import at.fhjoanneum.picturl.MAIN_ACTIVITY_RESULT_TAKE_PICTURE
 import at.fhjoanneum.picturl.R
 import at.fhjoanneum.picturl.db.PictUrlDatabase
 import at.fhjoanneum.picturl.model.PictUrlImage
 import at.fhjoanneum.picturl.ui.adapter.ImageClickListener
 import at.fhjoanneum.picturl.ui.adapter.ImagesListAdapter
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.leinardi.android.speeddial.SpeedDialView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity(), ImageClickListener {
     private lateinit var recyclerView: RecyclerView
+    private var photoURI: Uri = Uri.EMPTY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         recyclerView = findViewById(R.id.mainRecyclerView)
-        findViewById<FloatingActionButton>(R.id.mainActionButton).setOnClickListener {
+        val speedDialView = findViewById<SpeedDialView>(R.id.mainActionButton)
+        speedDialView.inflate(R.menu.menu_main_action)
+        speedDialView.setOnActionSelectedListener { actionItem ->
+            when(actionItem.id) {
+                R.id.actionFilePick -> dispatchPickImageIntent()
+                R.id.actionCamera -> dispatchTakePictureIntent()
+            }
+            false
+        }
+        loadImages()
+    }
+
+    private fun dispatchPickImageIntent() {
             val pickPhoto = Intent(
                 Intent.ACTION_OPEN_DOCUMENT,
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI
             ).setType("image/*")
-            startActivityForResult(pickPhoto, 1)
+            startActivityForResult(pickPhoto, MAIN_ACTIVITY_RESULT_PICK_IMAGE)
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    Log.e("err", "err", ex)
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    photoURI = FileProvider.getUriForFile(
+                        this,
+                        "at.fhjoanneum.picturl",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, MAIN_ACTIVITY_RESULT_TAKE_PICTURE)
+                }
+            }
         }
-        loadImages()
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        return File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        )
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -92,14 +149,16 @@ class MainActivity : AppCompatActivity(), ImageClickListener {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (data?.data != null) {
-            startActivity(
-                Intent(this, UploadActivity::class.java).putExtra(
-                    Intent.EXTRA_STREAM,
-                    data.data!!
-                )
-            )
+        var uri = photoURI
+        if (requestCode == MAIN_ACTIVITY_RESULT_PICK_IMAGE) {
+            uri = data?.data!!
         }
+        startActivity(
+            Intent(this, UploadActivity::class.java).putExtra(
+                Intent.EXTRA_STREAM,
+                uri
+            )
+        )
     }
 
     override fun onItemClicked(item: PictUrlImage) =
